177a63751996a68b31e9c002853654868bb8c423
AndyFilter <gmaciejg525@gmail.com>
2025-03-24 16:59:34 +0100

    Improved install.sh / uninstall.sh scripts This fix should help with transitioning to the newer driver version.

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Get the installed version of the driver
+installed_version=$(dkms status | grep -oP '^([l|y]eetmouse-driver[\/(, )]) ?\K([0-9.]+)')
+
+if [[ ! -z "$installed_version" ]] then
+	echo "Driver ($installed_version) already installed, exiting."
+	exit 0
+fi
+
 # Install the driver and activate the dkms module
 sudo make setup_dkms
 sudo dkms install -m yeetmouse-driver -v 0.9.2 # Enter the version you determined from the Makefile earlier in here
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+
+# Get the installed version of the driver
+installed_version=$(dkms status | grep -oP '^([l|y]eetmouse-driver[\/(, )]) ?\K([0-9.]+)')
+
+# Check if the driver is even installed
+if [[ -z "$installed_version" ]] then
+	echo "Driver not installed, exiting."
+	exit 0
+fi
+
+# Check if the installed version is old
+if [[ $(printf '%s\n' "$installed_version" "0.9.1" | sort -V | head -n1) == "$installed_version" ]]; then
+	echo "Please uninstall the old driver ($installed_version) using the old uninstaller first!"
+	exit 1
+fi
+
 # Uninstall the driver
-#sudo dkms remove -m yeetmouse-driver -v 0.9.2 # Newer dkms versions
-sudo dkms remove yeetmouse-driver/0.9.2 --all # Older (for Ubuntu <= 20.04), but should work now too
+#sudo dkms remove -m yeetmouse-driver -v $installed_version # Newer dkms versions
+sudo dkms remove "yeetmouse-driver/$installed_version" --all # Older (for Ubuntu <= 20.04), but should work now too
 sudo make remove_dkms
 sudo rmmod yeetmouse
